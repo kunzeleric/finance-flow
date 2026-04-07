904b89e7155fc2719a1181ff30b9a6d417328cf3
Eric Kunzel <kunzel.eric@gmail.com>
2026-04-06 22:59:19 -0300

    adjusting login form style

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -2,11 +2,15 @@ import { Field, FieldDescription, FieldLabel } from "@/src/components/ui/field";
 import { Button } from "@/src/components/ui/button";
 import { Input } from "@/src/components/ui/input";
 import { signIn } from "@/src/lib/auth";
+import {} from "lucide-react";
 
 export function LoginForm() {
   return (
-    <section className="flex flex-col gap-2 p-4 border border-white rounded-lg">
-      <h1 className="text-3xl mb-4">Bem vindo(a)!</h1>
+    <section className="flex flex-col gap-2 p-8 border border-white rounded-lg">
+      <div className="flex flex-col gap-6 mb-4">
+        <h1 className="text-3xl font-medium">Seja bem-vindo(a)!</h1>
+        <p>Faça seu login utilizando sua conta Google!</p>
+      </div>
       <form
         className="flex flex-col gap-4"
         action={async () => {
@@ -16,7 +20,7 @@ export function LoginForm() {
       >
         <Field>
           <FieldLabel htmlFor="email">E-mail</FieldLabel>
-          <Input type="email" name="email" placeholder="email@exemplo.com.br" />
+          <Input type="email" name="email" placeholder="seu-email@gmail.com" />
         </Field>
         <Button type="submit">Login</Button>
       </form>

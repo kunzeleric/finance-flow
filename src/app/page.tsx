9076e3906466fa4b7f86edbdafc880efe4e6d11f
Eric Kunzel <kunzel.eric@gmail.com>
2026-04-09 10:45:46 -0300

    chore: uninstall next-auth and installed supabase

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { LoginForm } from "@/src/components/login-form";
-
-export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center font-sans">
-      <LoginForm />
-    </div>
-  );
-}
